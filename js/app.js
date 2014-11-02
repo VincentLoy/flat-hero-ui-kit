03c3a9cb4d176d3b8916dbf6e0efac248a6d7a3b
Vincent Loy <vincent.loy1@gmail.com>
2014-11-02 17:53:49 +0100

    delete some useless code

--- js/app.js
+++ js/app.js
@@ -1,19 +1,5 @@
 (function ($) {
 	"use strict";
 
-	$(document).ready(init);
-   	function init(){
-        prettyPrint();
-        nav();
-    }
-    
-    function nav(){
-         var link = $(".side-nav li a");
-        link.on("click", function(){
-            var targetId = $(this).attr("href").replace("#","");
-            $('html, body').stop(true).animate({
-				scrollTop: $("#"+targetId).offset().top
-			}, 750, 'easeOutQuart');
-        });
-    }
+	//nothing here yet 
 })(jQuery)
